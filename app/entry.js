4f8bc44c76c55a6bc07db34869593d48c4877563
potapovDim <dmytrop@weblium.com>
2016-12-21 16:33:54 +0200

    remove

--- app/entry.js
+++
@@ -1,4 +0,0 @@
-import ReactDOM from 'react-dom'
-
-ReactDOM.render(routers,document.getElementById('app'))
-    
--- app/index.js
+++ app/index.js
@@ -6,13 +6,16 @@ class Counter extends React.Component{
   state = {
     count :1
   }
-
   render () {
     return (
       <div>
       HELLOOOO!!!!!!!!!!!!
       <button onClick={()=>this.setState({count:this.state.count+1})}>test</button>
       <h1>{this.state.count}</h1>
+       <canvas ref="canvas"
+          width={this.state.screen.width * this.state.screen.ratio}
+          height={this.state.screen.height * this.state.screen.ratio}
+        />
       </div>
     )
   }
